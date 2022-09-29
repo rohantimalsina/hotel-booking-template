5bc253f6c13c0a8031323f35c66e616eb39f9060
Rohan Timalsina <np03a190206@heraldcollege.edu.np>
2022-09-30 00:30:06 +0545

    navigation menu

---
+++ includes/navbar.php
@@ -0,0 +1,114 @@
+<body>
+
+<!-- HEADER -->
+<header class="header-sky">
+    <div class="container">
+        <!--HEADER-TOP-->
+        <div class="header-top">
+            <div class="header-top-left">
+                <span><p>Welcome to DMatel Hotels and Resorts</p></span>
+            </div>
+            <div class="header-top-right">
+                <ul>
+                    <li class="dropdown"><a href="login.html" title="LOGIN" class="dropdown-toggle">HOME</a></li>
+                    <li class="dropdown"><a href="register.html" title="REGISTER" class="dropdown-toggle">ABOUT US</a></li>
+                    <li class="dropdown"><a href="register.html" title="REGISTER" class="dropdown-toggle">WHY DMT</a></li>
+                    <li class="dropdown"><a href="register.html" title="REGISTER" class="dropdown-toggle">ROOMS</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- END/HEADER-TOP -->
+    </div>
+ 
+ 
+ <!-- MENU-HEADER -->
+ <div class="menu-header">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-header ">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar "></span>
+                            <span class="icon-bar "></span>
+                            <span class="icon-bar "></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php" title="Skyline"><img src="images/logo.png" alt="#"></a>
+                    </div>
+                    <div class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="dropdown ">
+                                <a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Victoria Island<b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="index.html" title="">Home 1</a></li>
+                                    <li><a href="index_2.html" title="">Home 2</a></li>
+                                    <li><a href="index_3.html" title="">Home 3</a></li>
+                                    <li><a href="index_4.html" title="">Home 4</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="room_1.html" class="dropdown-toggle" data-toggle="dropdown">Agungi, Lekki<b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="room_1.html" title="">Room 1</a></li>
+                                    <li><a href="room_2.html" title="">Room 2</a></li>
+                                    <li><a href="room_3.html" title="">Room 3</a></li>
+                                    <li><a href="room_4.html" title="">Room 4</a></li>
+                                    <li><a href="room_5.html" title="">Room 5</a></li>
+                                    <li><a href="room_6.html" title="">Room 6</a></li>
+                                    <li><a href="room_detail.html" title="">Room Detail</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ikota<b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="guestbook.html" title="">Guest Book</a></li>
+                                    <li><a href="attractions.html" title="">Attractions</a></li>
+                                    <li><a href="404_page.html" title="">404 Page</a></li>
+                                    <li><a href="comming_soon.html" title="">Comming Soon</a></li>
+                                    <li><a href="check_out.html" title="">Check Out</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="reservation_1.html" class="dropdown-toggle" data-toggle="dropdown">Rumumasi, PH<b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="reservation_1.html" title="">Reservation 1</a></li>
+                                    <li><a href="reservation_2.html" title="">Reservation 2</a></li>
+                                    <li><a href="reservation_3.html" title="">Reservation 3</a></li>
+                                    <li><a href="reservation_4.html" title="">Reservation 4</a></li>
+                                    <li><a href="reservation_5.html" title="">Reservation 5</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="gallery_1.html" class="dropdown-toggle" data-toggle="dropdown">GRA Phase 2 PH <b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="gallery_1.html" title="">Gallery 1</a></li>
+                                    <li><a href="gallery_2.html" title="">Gallery 2</a></li>
+                                    <li><a href="gallery_3.html" title="">Gallery 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="gallery_1.html" class="dropdown-toggle" data-toggle="dropdown">Ind. L/O Enugu <b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="gallery_1.html" title="">Gallery 1</a></li>
+                                    <li><a href="gallery_2.html" title="">Gallery 2</a></li>
+                                    <li><a href="gallery_3.html" title="">Gallery 3</a></li>
+
+                                </ul>
+                            </li>
+                            <li class="dropdown ">
+                                <a href="gallery_1.html" class="dropdown-toggle" data-toggle="dropdown">New Hotel <b class="caret"></b></a>
+                                <ul class="dropdown-menu icon-fa-caret-up submenu-hover">
+                                    <li><a href="gallery_1.html" title="">Gallery 1</a></li>
+                                    <li><a href="gallery_2.html" title="">Gallery 2</a></li>
+                                    <li><a href="gallery_3.html" title="">Gallery 3</a></li>
+
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <!-- END / MENU-HEADER -->
+    </header>
+    <!-- END-HEADER -->
